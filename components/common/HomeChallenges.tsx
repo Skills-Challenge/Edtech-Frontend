4a69bd63d96bf1fda8d7c41d90be64ed5f090c4a
RUKUNDO Prince <rukundoprince951@gmail.com>
2025-01-27 21:54:12 +0200

    some minor changes

--- components/common/HomeChallenges.tsx
+++ components/common/HomeChallenges.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const HomeChallenges = () => {
   return (
-    <section className='flex flex-col items-center xl:p-52 lg:p-38 md:p-16 p-5 text-[#03192E] gap-2'>
+    <section className='flex flex-col items-center xl:px-52 lg:px-38 md:px-16 px-5 text-[#03192E] gap-2 md:mt-10'>
       <h1 className='font-bold lg:text-4xl md:text-3xl text-2xl'>Explore Challenges & Hackathons </h1>
       <p className='md:w-[60%] text-center mx-auto text-sm md:text-medium lg:text-lg'>Join Skills Challenges Program to accelerate your career growth and become part of Africa’s largest workforce of digital professionals. </p>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-[26px] pt-[18px]">
@@ -19,7 +19,7 @@ const HomeChallenges = () => {
           />
         ))}
       </div>
-      <Link href="/challenges" className='border-2 border-primary py-2 px-10 rounded-md mt-5'>View More</Link>
+      <Link href="/challenges" className='border-2 border-blue-600 py-2 px-10 rounded-md mt-5 text-blue-600'>View More</Link>
     </section>
   )
 }
